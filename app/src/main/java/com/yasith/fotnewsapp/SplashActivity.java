690e571e928346c2fc7d40cead13811f0af833d1
yasith uthpala <167942712+yasith-uthpala@users.noreply.github.com>
2025-06-03 22:21:11 +0530

    Developed Splash Screen

---
+++ app/src/main/java/com/yasith/fotnewsapp/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.yasith.fotnewsapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DURATION = 2000; // 2 seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Go to SigninActivity after splash
+                Intent intent = new Intent();
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DURATION);
+    }
+}
